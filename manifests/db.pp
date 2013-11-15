dc2230481ca23295833a376f7b9ec311f84223ec
Rodrigo Russo <rodrigozrusso@gmail.com>
2013-11-15 17:08:28 -0200

    adding create opencart db

--- manifests/db.pp
+++ manifests/db.pp
@@ -6,8 +6,24 @@ package { "mysql-server":
 file {"/etc/mysql/conf.d/allow_external.cnf":
 	owner	=> mysql,
 	group	=> mysql,
-	mode	=> 0644,
+	mode	=> 0644,	
 	require => Package["mysql-server"],
 	content => template("/vagrant/allow_external.cnf"),
+	notify  => Service["mysql"],
 }
 
+service { "mysql":
+	ensure     => running,
+	enable     => true,
+	hasstatus  => true,
+	hasrestart => true,
+	require    => Package["mysql-server"],
+}
+
+exec { "create-opencart-db":
+
+	command     => "mysqladmin -uroot create opencart",
+	unless      => "mysqlshow -uroot opencart",
+	path		=> "/usr/bin/",
+	require    => Package["mysql-server"],
+}
