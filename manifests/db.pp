5359b7d909d6ff822ba186f6ef791c5017163562
Rodrigo Russo <rodrigozrusso@gmail.com>
2013-11-14 22:02:51 -0200

    adding mysql user

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,3 +1,10 @@
 package { "mysql-server":
     ensure => installed,
 }
+
+file {"/etc/mysql/conf.d/allow_external.cnf":
+	owner	=> mysql,
+	group	=> mysql,
+	mode	=> 0644,
+	content	=> "[mysqld]\n	bind-address = 0.0.0.0",
+}
