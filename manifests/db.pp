9da226a7e96edcf0dfd460f992d74ff167ad9cdc
Rodrigo Russo <rodrigozrusso@gmail.com>
2013-11-15 15:46:03 -0200

    Adding require

--- manifests/db.pp
+++ manifests/db.pp
@@ -7,6 +7,7 @@ file {"/etc/mysql/conf.d/allow_external.cnf":
 	owner	=> mysql,
 	group	=> mysql,
 	mode	=> 0644,
+	require => Package["mysql-server"],
 	content => template("/vagrant/allow_external.cnf"),
 }
 
