94aeac27646edf6d56f1f3e4b94378a684a2bf3a
Rodrigo Russo <rodrigozrusso@gmail.com>
2013-11-15 18:00:22 -0200

    refactoring db.pp to use define types

--- manifests/db.pp
+++ manifests/db.pp
@@ -1,36 +1,51 @@
+class mysql {
 
-package { "mysql-server":
-    ensure => installed,
+	package { "mysql-server":
+	    ensure => installed,
+	}
+
+	file {"/etc/mysql/conf.d/allow_external.cnf":
+		owner	=> mysql,
+		group	=> mysql,
+		mode	=> 0644,	
+		require => Package["mysql-server"],
+		content => template("/vagrant/allow_external.cnf"),
+		notify  => Service["mysql"],
+	}
+
+	service { "mysql":
+		ensure     => running,
+		enable     => true,
+		hasstatus  => true,
+		hasrestart => true,
+		require    => Package["mysql-server"],
+	}
+
 }
 
-file {"/etc/mysql/conf.d/allow_external.cnf":
-	owner	=> mysql,
-	group	=> mysql,
-	mode	=> 0644,	
-	require => Package["mysql-server"],
-	content => template("/vagrant/allow_external.cnf"),
-	notify  => Service["mysql"],
+include mysql
+
+
+define opencart_db($database, $username, $password) {
+	
+	exec { "create-opencart-db":
+		command     => "mysqladmin -uroot create opencart",
+		unless      => "mysqlshow -uroot opencart",
+		path		=> "/usr/bin/",
+		require    => Service["mysql"],
+	}
+
+	exec { "grant-opencart-user":
+
+		command     => "mysql -uroot -e \"grant all on $database.* to '$username'@'%' identified by '$password';grant all on $database.* to '$username'@'localhost' identified by '$password';\"",
+		unless      => "mysql -u$username -p$password",
+		path		=> "/usr/bin/",
+		require		=> Exec["create-opencart-db"],
+	}
 }
 
-service { "mysql":
-	ensure     => running,
-	enable     => true,
-	hasstatus  => true,
-	hasrestart => true,
-	require    => Package["mysql-server"],
-}
-
-exec { "create-opencart-db":
-	command     => "mysqladmin -uroot create opencart",
-	unless      => "mysqlshow -uroot opencart",
-	path		=> "/usr/bin/",
-	require    => Service["mysql"],
-}
-
-exec { "grant-opencart-user":
-
-	command     => "mysql -uroot -e \"grant all on opencart.* to 'opencart'@'%' identified by 'openpass';grant all on opencart.* to 'opencart'@'localhost' identified by 'openpass';\"",
-	unless      => "mysql -uopencart -popenpass",
-	path		=> "/usr/bin/",
-	require		=> Exec["create-opencart-db"],
+opencart_db { "opencart-db-parameters":
+  database => "opencart",
+  username => "openpass",
+  password => "openpass",
 }
